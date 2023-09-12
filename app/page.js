b3559f2186176d8771540730efd0bfe5aa3fdf16
Arihant Jain <arihantj916@gmail.com>
2023-09-12 23:17:52 +0530

    optmize and improve code

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import { LogoutButton } from "../components/user/logout";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Arihant</h1>
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <h1 className="text-6xl font-bold">Arihant Jain's Home Page</h1>
       <LogoutButton />
     </main>
   );
--- components/user/logout.jsx
+++ components/user/logout.jsx
@@ -1,10 +1,23 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import Link from "next/link";
 
 export const LogoutButton = () => {
+  const [isToken, setToken] = useState(false);
   const router = useRouter();
-  const Logout = async () => {
+
+  useEffect(() => {
+    // Check for the JWT token when the component mounts
+    const token = Cookies.get("JWT_AUTH_TOKEN");
+    if (token) {
+      setToken(true);
+    }
+  }, []);
+
+  const handleLogout = async () => {
     try {
       const response = await fetch("/api/user/logout/route", {
         method: "POST",
@@ -24,5 +37,17 @@ export const LogoutButton = () => {
       console.error("Error:", error);
     }
   };
-  return <button onClick={Logout}>Logout</button>;
+
+  return (
+    <div>
+      {isToken ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <div>
+          <p>You are not logged in!</p>
+          <p className="p-4 mt-3 text-center text-white bg-blue-700 rounded-full"><Link href="/user/login">Login</Link></p>
+        </div>
+      )}
+    </div>
+  );
 };
