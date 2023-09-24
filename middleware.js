ba9a0d2f204a66903ebd7ed31d33e566dc0f0a26
Arihant Jain <arihantj916@gmail.com>
2023-09-24 18:05:48 +0530

    Update Middleware

--- middleware.js
+++ middleware.js
@@ -4,16 +4,16 @@ export function middleware(request) {
   const Path = request.nextUrl.pathname;
   const PublicPath = Path === "/user/login" || Path === "/user/signup";
 
-  const token = request.cookies.get();
+  const token = request.cookies.get("JWT_AUTH_TOKEN");
 
   if (PublicPath && token) {
     return NextResponse.redirect(new URL("/", request.nextUrl));
   }
-  if(!PublicPath && !token){
-    return NextResponse.redirect(new URL("/user/login", request.nextUrl));
-  }
+  // if(!PublicPath && !token){
+  //   return NextResponse.redirect(new URL("/user/login", request.nextUrl));
+  // }
 }
 
 export const config = {
-  matcher: ["/", "/user/login", "/user/signup"],
+  matcher: ["/","/user/login", "/user/signup"],
 };
